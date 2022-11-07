97c34ef76e6051c9797ad0b157bad331280516f6
Gustavo <g.rezendesilva@tudelft.nl>
2022-11-07 11:39:34 +0100

    🎨 using autopep8 to format code

--- launch/pipeline_env.launch.py
+++ launch/pipeline_env.launch.py
@@ -8,61 +8,61 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-	fcu_url_arg = LaunchConfiguration('fcu_url')
-	gcs_url_arg = LaunchConfiguration('gcs_url')
-	system_id_arg = LaunchConfiguration('system_id')
-	component_id_arg = LaunchConfiguration('component_id')
-	tgt_system_arg = LaunchConfiguration('target_system_id')
-	tgt_component_arg = LaunchConfiguration('target_component_id')
-	
-	fcu_url_arg = DeclareLaunchArgument(
-		'fcu_url',
-		default_value='udp://:14551@:14555'
-		)
-	
-	gcs_url_arg = DeclareLaunchArgument(
-		'gcs_url',
-		default_value='udp://@localhost:14550'
-		)
+    fcu_url_arg = LaunchConfiguration('fcu_url')
+    gcs_url_arg = LaunchConfiguration('gcs_url')
+    system_id_arg = LaunchConfiguration('system_id')
+    component_id_arg = LaunchConfiguration('component_id')
+    tgt_system_arg = LaunchConfiguration('target_system_id')
+    tgt_component_arg = LaunchConfiguration('target_component_id')
 
-	system_id_arg = DeclareLaunchArgument(
-		'system_id',
-		default_value='255'
-		)
+    fcu_url_arg = DeclareLaunchArgument(
+        'fcu_url',
+        default_value='udp://:14551@:14555'
+    )
 
-	component_id_arg = DeclareLaunchArgument(
-		'component_id',
-		default_value='240'
-		)
+    gcs_url_arg = DeclareLaunchArgument(
+        'gcs_url',
+        default_value='udp://@localhost:14550'
+    )
 
-	tgt_system_arg = DeclareLaunchArgument(
-		'target_system_id',
-		default_value='1'
-		)
+    system_id_arg = DeclareLaunchArgument(
+        'system_id',
+        default_value='255'
+    )
 
-	tgt_component_arg = DeclareLaunchArgument(
-		'target_component_id',
-		default_value='1'
-		)
+    component_id_arg = DeclareLaunchArgument(
+        'component_id',
+        default_value='240'
+    )
 
-	mavros_node = Node(
+    tgt_system_arg = DeclareLaunchArgument(
+        'target_system_id',
+        default_value='1'
+    )
+
+    tgt_component_arg = DeclareLaunchArgument(
+        'target_component_id',
+        default_value='1'
+    )
+
+    mavros_node = Node(
         package='mavros',
         executable='mavros_node',
         parameters=[{
-		'fcu_url': LaunchConfiguration('fcu_url'),
-		'gcs_url': LaunchConfiguration('gcs_url'),
-		'system_id': LaunchConfiguration('system_id'),
-		'component_id': LaunchConfiguration('component_id'),
-		'target_system_id': LaunchConfiguration('target_system_id'),
-		'target_component_id': LaunchConfiguration('target_component_id'),
-		}]
-        )
-	return LaunchDescription([
-		fcu_url_arg,
-		gcs_url_arg,
-		system_id_arg,
-		component_id_arg,
-		tgt_system_arg,
-		tgt_component_arg,
-		mavros_node
-		])
+            'fcu_url': LaunchConfiguration('fcu_url'),
+            'gcs_url': LaunchConfiguration('gcs_url'),
+            'system_id': LaunchConfiguration('system_id'),
+            'component_id': LaunchConfiguration('component_id'),
+            'target_system_id': LaunchConfiguration('target_system_id'),
+            'target_component_id': LaunchConfiguration('target_component_id'),
+        }]
+    )
+    return LaunchDescription([
+        fcu_url_arg,
+        gcs_url_arg,
+        system_id_arg,
+        component_id_arg,
+        tgt_system_arg,
+        tgt_component_arg,
+        mavros_node
+    ])
