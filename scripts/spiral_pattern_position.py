3f813220142e93fafadb6f61fd707cfc148c440d
Jeroen <j.zwanepol@hotmail.com>
2022-11-08 16:53:38 +0100

    added rosparam for spiral_width

--- scripts/spiral_pattern_position.py
+++ scripts/spiral_pattern_position.py
@@ -11,10 +11,6 @@ from mavros_msgs.srv import CommandBool, SetMode
 
 from mavros_wrapper.ardusub_wrapper import *
 
-def check_setpoint_reached(a, x, y, delta=0.1):
-    return abs(a.x - x) <= delta \
-            and abs(a.y - y) <= delta
-
 def spiral_points(i, resolution=0.1, spiral_width=1.0):
     step_idx = resolution * i
     x = np.cos(step_idx) * spiral_width * step_idx 
@@ -40,19 +36,22 @@ def main(args=None):
         sp_publisher.set_mode(custom_mode)
         sp_publisher.rate.sleep()
 
+
+    sp_publisher.declare_parameter('spiral_width',1.0)
+
     i = 0
     # Ensure that UUV is at start point
     x = .0
     y = .0
-    sp_publisher.setpoint_position_local(x,y,.0,.0,.0,.0,1.0)
+    z = sp_publisher.local_pos.pose.position.z
     try:
         while rclpy.ok():
-            if check_setpoint_reached(sp_publisher.local_pos.pose.position,x,y):
-                print(i,'th iteration')
-                print('x pose goal = ', x, ' y pose goal = ', y)
-                x,y = spiral_points(i, resolution=0.1, spiral_width=1.0)
-                sp_publisher.setpoint_position_local(x,y,.0,.0,.0,.0,1.0)
+            spiral_width = sp_publisher.get_parameter('spiral_width').get_parameter_value().double_value
+            if sp_publisher.check_setpoint_reached(sp_publisher.pose_stamped(x,y,z),
+                    delta=0.2):
+                x,y = spiral_points(i, resolution=0.1,  spiral_width=spiral_width)
                 i += 1 
+            sp_publisher.setpoint_position_local(x,y,z)
             sp_publisher.rate.sleep()
     except KeyboardInterrupt:
         pass
