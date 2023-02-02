af713224e73432545e59050633ff7d70d8dd7111
Gustavo <g.rezendesilva@tudelft.nl>
2023-02-02 17:43:13 +0100

    ⚡ improving performance and design

--- suave/suave/task_bridge.py
+++ suave/suave/task_bridge.py
@@ -16,6 +16,7 @@ class TaskBridge(Node):
         self.declare_parameter('f_follow_pipeline_mode', 'fd_follow_pipeline')
 
         client_cb_group = MutuallyExclusiveCallbackGroup()
+        task_cb_group = MutuallyExclusiveCallbackGroup()
         self.generate_path_sm_cli = self.create_client(
             ChangeMode,
             '/f_generate_search_path/change_mode',
@@ -26,38 +27,46 @@ class TaskBridge(Node):
             '/f_follow_pipeline/change_mode',
             callback_group=client_cb_group)
 
-        self.sm_cli_dict = {
-            'generate_search_path': self.generate_path_sm_cli,
-            'follow_pipeline': self.follow_pipeline_sm_cli,
-        }
-
         self.task_request_service = self.create_service(
             Task,
             'task/request',
-            self.task_request_cb)
+            self.task_request_cb,
+            callback_group=task_cb_group)
+
+        self.sm_cli_dict = {
+            'f_generate_search_path': self.generate_path_sm_cli,
+            'f_follow_pipeline': self.follow_pipeline_sm_cli,
+        }
+
+        self.task_functions_dict = {
+            'search_pipeline': ['f_generate_search_path'],
+            'inspect_pipeline': ['f_follow_pipeline'],
+        }
 
     def task_request_cb(self, req, response):
+        response = Task.Response()
         try:
-            mode_name = self.get_parameter(
-                'f_' + req.task_name + '_mode').value
+            function_names = self.task_functions_dict[req.task_name]
+            success = True
+            for function in function_names:
+                mode_name = self.get_parameter(function + '_mode').value
+                cli = self.sm_cli_dict[function]
+                while not cli.wait_for_service(timeout_sec=1.0):
+                    self.get_logger().info(
+                       'service not available, waiting again...')
+
+                mode_req = ChangeMode.Request()
+                mode_req.mode_name = mode_name
+                mode_res = cli.call(mode_req)
+                success = mode_res.success and success
+            response.success = success
         except Exception as e:
             self.get_logger().error(
-                str(e) + '. Probably requested wrong task name.')
+                str(e) + '. Probably requested wrong task name'
+                + 'Available tasks are {}'.format(self.sm_cli_dict.keys()))
             response = Task.Response()
             response.success = False
-            return response
 
-        cli = self.sm_cli_dict[req.task_name]
-        while not cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again...')
-
-        mode_req = ChangeMode.Request()
-        mode_req.mode_name = mode_name
-        future = cli.call_async(mode_req)
-        timer = self.create_rate(5)
-        rclpy.spin_until_future_complete(self, future)
-        response = Task.Response()
-        response.success = future.result().success
         return response
 
 
