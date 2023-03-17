41480c167d03e9bdcb8780ad3620d8366b1bb22c
Gustavo <g.rezendesilva@tudelft.nl>
2023-03-17 18:39:42 +0100

    🚧 refactoring/improving mission planner

--- suave_missions/suave_missions/mission_planner.py
+++ suave_missions/suave_missions/mission_planner.py
@@ -2,23 +2,53 @@ import csv
 import rclpy
 from pathlib import Path
 from datetime import datetime
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool
+from mavros_msgs.srv import SetMode
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.node import Node
-from suave.bluerov_gazebo import BlueROVGazebo
 from std_msgs.msg import Bool
-from system_modes_msgs.srv import ChangeMode
+from suave_msgs.srv import Task
 
 
-class MissionPlanner(BlueROVGazebo):
+class MissionPlanner(Node):
     def __init__(self, node_name='mission_node'):
         super().__init__(node_name)
 
+        self.status = State()
+        self.state_sub = self.create_subscription(
+            State,
+            'mavros/state',
+            self.status_cb,
+            10,
+            callback_group=MutuallyExclusiveCallbackGroup()
+        )
+
         self.pipeline_detected = False
         self.pipeline_detected_sub = self.create_subscription(
-            Bool, 'pipeline/detected', self.pipeline_detected_cb, 10)
+            Bool,
+            'pipeline/detected',
+            self.pipeline_detected_cb,
+            10,
+            callback_group=MutuallyExclusiveCallbackGroup())
 
         self.pipeline_inspected = False
         self.pipeline_inspected_sub = self.create_subscription(
-            Bool, 'pipeline/inspected', self.pipeline_inspected_cb, 10)
+            Bool,
+            'pipeline/inspected',
+            self.pipeline_inspected_cb,
+            10,
+            callback_group=MutuallyExclusiveCallbackGroup())
+
+        self.arm_motors_service = self.create_client(
+            CommandBool, 'mavros/cmd/arming')
+        self.set_mode_service = self.create_client(
+            SetMode, 'mavros/set_mode')
+
+        self.task_request_service = self.create_client(
+            Task, 'task/request')
+        self.task_cancel_service = self.create_client(
+            Task, 'task/cancel')
 
         self.declare_parameter('result_path', '~/suave/results')
         self.declare_parameter('result_filename', 'mission_results')
@@ -34,11 +64,49 @@ class MissionPlanner(BlueROVGazebo):
 
         self.metrics_header = ['mission_name', 'datetime', 'metric']
 
+        self.pipeline_detected_time = -1
+        self.abort_mission = False
+
+    def status_cb(self, msg):
+        self.status = msg
+
     def pipeline_detected_cb(self, msg):
         self.pipeline_detected = msg.data
+        if self.pipeline_detected is True:
+            self.pipeline_detected_time = self.get_clock().now()
+        self.destroy_subscription(self.pipeline_detected_sub)
 
     def pipeline_inspected_cb(self, msg):
         self.pipeline_inspected = msg.data
+
+    def arm_motors(self, arm_motors_bool):
+        req = CommandBool.Request()
+        req.value = arm_motors_bool
+        return self.call_service(self.arm_motors_service, req)
+
+    def set_mode(self, mode):
+        req = SetMode.Request()
+        req.custom_mode = mode
+        return self.call_service(self.set_mode_service, req)
+
+    def request_task(self, task_name):
+        req = Task.Request()
+        req.task_name = task_name
+        return self.call_service(self.task_request_service, req)
+
+    def cancel_task(self, task_name):
+        req = Task.Request()
+        req.task_name = task_name
+        return self.call_service(self.task_cancel_service, req)
+
+    def call_service(self, cli, request):
+        while not cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        future = cli.call_async(request)
+        while self.executor.spin_until_future_complete(
+                future, timeout_sec=1.0):
+            self.get_logger().info("Waiting for future to complete")
+        return future.result()
 
     def save_metrics(self, data):
         result_path = Path(self.result_path).expanduser()
@@ -61,12 +129,16 @@ class MissionPlanner(BlueROVGazebo):
     def perform_mission(self):
         self.get_logger().warning("No mission defined!!!")
 
-    def manual_sysmode_change(self, mode_name, cli):
-        req = ChangeMode.Request()
-        req.mode_name = mode_name
-
-        if (isinstance(cli, list)):
-            for client in cli:
-                client.call_async(req)
-        else:
-            cli.call_async(req)
+    def perform_task(self, task_name, condition):
+        self.get_logger().info('Starting {} task'.format(task_name))
+        self.task_timer = self.create_rate(1)
+        task_status = "completed"
+        if self.abort_mission is False:
+            self.request_task(task_name)
+            while condition() is not True:
+                if self.abort_mission is True:
+                    task_status = "aborted"
+                    break
+                self.task_timer.sleep()
+        self.get_logger().info('Task {0} {1}'.format(task_name, task_status))
+        self.cancel_task(task_name)
--- suave_missions/suave_missions/time_constrained_mission.py
+++ suave_missions/suave_missions/time_constrained_mission.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python
 import sys
 import rclpy
-import threading
 import os
+
 from datetime import datetime
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from suave_missions.mission_planner import MissionPlanner
-from system_modes_msgs.srv import ChangeMode
-
 from std_msgs.msg import Float32
-
-from rclpy.action import ActionClient
-from rclpy.executors import MultiThreadedExecutor
-from mros2_msgs.action import ControlQos
-from diagnostic_msgs.msg import DiagnosticArray
-from diagnostic_msgs.msg import DiagnosticStatus
-from diagnostic_msgs.msg import KeyValue
+from suave_missions.mission_planner import MissionPlanner
 
 
 class MissionTimeConstrained(MissionPlanner):
@@ -30,41 +22,17 @@ class MissionTimeConstrained(MissionPlanner):
 
         self.declare_parameter('time_limit', 300)
         self.time_limit = self.get_parameter('time_limit').value
-        self.get_logger().info('\n\n\n!!!TIME LIMIT:' + str(self.time_limit))
-
-        self.declare_parameter('f_generate_search_path_mode', 'fd_spiral_low')
-        self.declare_parameter('f_follow_pipeline_mode', 'fd_follow_pipeline')
-
-        self.chosen_search_mode = self.get_parameter(
-            'f_generate_search_path_mode').value
-        self.chosen_inspect_mode = self.get_parameter(
-            'f_follow_pipeline_mode').value
-
-        self.get_logger().info(
-            '\n\n\nAdaptation Manager:' + str(self.adaptation_manager))
 
         self.pipeline_distance_inspected_sub = self.create_subscription(
             Float32,
             'pipeline/distance_inspected',
             self.distance_inspected_cb,
-            1)
+            1,
+            callback_group=MutuallyExclusiveCallbackGroup())
 
-        self.abort_mission = False
-
-        self.using_no_adaptation = self.adaptation_manager == 'none'
         self.mission_start_time = None
         self.pipeline_detected_time = None
         self.distance_inspected = 0.0
-
-        self.generate_path_sm_cli = self.create_client(
-            ChangeMode,
-            '/f_generate_search_path/change_mode')
-
-        self.inspect_pipeline_sm_cli = self.create_client(
-            ChangeMode,
-            '/f_follow_pipeline/change_mode')
-
-        self.get_logger().info('TIME MISSION')
 
     def distance_inspected_cb(self, msg):
         self.distance_inspected = msg.data
@@ -74,16 +42,15 @@ class MissionTimeConstrained(MissionPlanner):
         elapsed_time = current_time - self.mission_start_time
         if elapsed_time.to_msg().sec >= self.time_limit:
             self.abort_mission = True
-            if self.using_no_adaptation:
-                self.manual_sysmode_change(
-                    "fd_unground",
-                    [self.generate_path_sm_cli, self.inspect_pipeline_sm_cli])
             detection_time_delta = -1
             if self.pipeline_detected_time is not None:
                 detection_time_delta = \
                     self.pipeline_detected_time - self.mission_start_time
                 detection_time_delta = detection_time_delta.to_msg().sec
 
+            # TODO: get this automatically
+            self.initial_x = 1.0
+            self.initial_y = 1.0
             mission_metrics = [
                 self.mission_name,
                 datetime.now().strftime("%b-%d-%Y-%H-%M-%S"),
@@ -96,41 +63,6 @@ class MissionTimeConstrained(MissionPlanner):
             self.get_logger().info('Time limited reached. Mission aborted!')
             os.system("touch ~/suave_ws/mission.done")
             self.time_monitor_timer.destroy()
-
-    def search_task(self):
-        if self.abort_mission is False:
-            if self.using_no_adaptation:
-                self.get_logger().info(
-                    'Sanity check, should be none as adaptation')
-
-                self.manual_sysmode_change(
-                    self.chosen_search_mode, self.generate_path_sm_cli)
-            while not self.pipeline_detected:
-                if self.abort_mission is True:
-                    return
-                self.timer.sleep()
-        else:
-            return
-
-        self.pipeline_detected_time = self.get_clock().now()
-
-        self.get_logger().info('Task Search Pipeline completed')
-
-    def inspect_task(self):
-        self.get_logger().info('Starting Inspect Pipeline task')
-        if self.abort_mission is False:
-            if self.using_no_adaptation:
-                self.manual_sysmode_change(
-                    self.chosen_inspect_mode,
-                    self.inspect_pipeline_sm_cli)
-            while not self.pipeline_inspected:
-                if self.abort_mission is True:
-                    return
-                self.timer.sleep()
-        else:
-            return
-
-        self.get_logger().info('Task Inspect Pipeline completed')
 
     def perform_mission(self):
         self.get_logger().info("Pipeline inspection mission starting!!")
@@ -145,42 +77,30 @@ class MissionTimeConstrained(MissionPlanner):
         guided_mode = 'GUIDED'
         while self.status.mode != guided_mode:
             self.get_logger().info(
-                'BlueROV mode is : {} waiting for mode change to register'.format(self.status.mode))
+                'BlueROV mode is : {} waiting for GUIDED mode'.format(
+                 self.status.mode))
             self.set_mode(guided_mode)
             self.timer.sleep()
-
-        self.get_logger().info('Starting Search Pipeline task')
 
         self.mission_start_time = self.get_clock().now()
         self.time_monitor_timer = self.create_timer(0.5, self.time_monitor_cb)
 
-        self.search_task()
-        if self.using_no_adaptation:
-            self.manual_sysmode_change(
-                'fd_unground', self.generate_path_sm_cli)
-
-        self.inspect_task()
-        if self.using_no_adaptation:
-            self.manual_sysmode_change(
-                'fd_unground', self.inspect_pipeline_sm_cli)
+        self.perform_task('search_pipeline', lambda: self.pipeline_detected)
+        self.perform_task('inspect_pipeline', lambda: self.pipeline_inspected)
 
         os.system("touch ~/suave_ws/mission.done")
 
 
 def main():
-
     rclpy.init(args=sys.argv)
 
     mission_node = MissionTimeConstrained()
 
     mt_executor = MultiThreadedExecutor()
-    thread = threading.Thread(
-        target=rclpy.spin, args=[mission_node, mt_executor], daemon=True)
-    thread.start()
+    mt_executor.add_node(mission_node)
+    mt_executor.create_task(mission_node.perform_mission)
+    mt_executor.spin()
 
-    mission_node.perform_mission()
-
-    thread.join()
     mission_node.destroy_node()
     rclpy.shutdown()
 
