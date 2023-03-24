3330948ee47a165e98d2cb454b9c2a68416832ec
Gustavo <g.rezendesilva@tudelft.nl>
2023-03-24 14:33:53 +0100

    🐋 updating dockerfiles

--- docker/src/ubuntu/install/suave/install_suave_deps.sh
+++ docker/src/ubuntu/install/suave/install_suave_deps.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-mkdir -p ~/suave_ws/src
-cd ~/suave_ws
-
-wget https://raw.githubusercontent.com/kas-lab/suave/main/suave/suave.rosinstall
-vcs import src < suave.rosinstall --recursive
-
-# echo 'export GZ_SIM_RESOURCE_PATH=$HOME/suave_ws/src/bluerov2_ignition/models:$HOME/suave_ws/src/bluerov2_ignition/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
-# echo 'export GZ_SIM_RESOURCE_PATH=$HOME/suave_ws/src/remaro_worlds/models:$HOME/suave_ws/src/remaro_worlds/worlds:${GZ_SIM_RESOURCE_PATH}' >> ~/.bashrc
-
 source /opt/ros/humble/setup.bash
 cd ~/suave_ws/
 
