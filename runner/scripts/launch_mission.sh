c18125864877c8d1b116b5b114ba378b4bea6738
Gustavo <g.rezendesilva@tudelft.nl>
2024-06-04 18:52:42 +0100

    ✨ add support for bt in the runner

--- runner/scripts/launch_mission.sh
+++ runner/scripts/launch_mission.sh
@@ -5,14 +5,14 @@ source ~/suave_ws/install/setup.bash
 if [ $# -lt 1 ]; then
 	echo "usage: $0 adaptation_manager mission_type"
 	echo example:
-	echo "  "$0 "[metacontrol | random | none] [time | distance]"
+	echo "  "$0 "[metacontrol | random | none | bt] [time | distance]"
 	exit 1
 fi
 
 MANAGER=""
 MTYPE=""
 
-if [ "$1" == "metacontrol" ] || [ "$1" == "random" ] || [[ "$1" == "none" ]];
+if [ "$1" == "metacontrol" ] || [ "$1" == "random" ] || [[ "$1" == "none" ]] || [[ "$1" == "bt" ]];
 then
     MANAGER=$1
 else
@@ -33,4 +33,13 @@ else
     fi
 fi
 
-ros2 launch suave_missions mission.launch.py adaptation_manager:=$MANAGER mission_type:=$MTYPE result_filename:=$3
+FILE=$3
+MCFILE=${FILE}"_mc_reasoning_time"
+
+if [ "$MANAGER" == "metacontrol" ] || [ "$MANAGER" == "random" ] || [[ "$MANAGER" == "none" ]];
+then
+    ros2 launch suave_missions mission.launch.py adaptation_manager:=$MANAGER mission_type:=$MTYPE result_filename:=$FILE mc_reasoning_time_filename:=$MCFILE
+elif [ "$MANAGER" == "bt" ];
+then
+  ros2 launch suave_bt suave_bt.launch.py result_filename:=$3
+fi
