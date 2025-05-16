36175436a46fedef00306bcb1ca948992aeeef6a
Gustavo <g.rezendesilva@tudelft.nl>
2025-05-16 19:21:08 +0200

    🔧 adjust setup.py and package.xml

--- suave_managing/suave_random/setup.py
+++ suave_managing/suave_random/setup.py
@@ -1,13 +1,13 @@
 import os
 from glob import glob
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'suave_random'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -17,7 +17,7 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='gus',
+    maintainer='Gustavo Rezende',
     maintainer_email='g.rezendesilva@tudelft.nl',
     description='Implementation of a random managing system for SUAVE',
     license='Apache-2.0',
